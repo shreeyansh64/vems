83f09c6804ed7e7c6021099aeea1aab808ed026a
shreeyansh64 <shreeyansh64@gmail.com>
2026-06-21 21:26:21 +0530

    login: add login state for bloc

---
+++ lib/features/auth/presentation/bloc/login_state.dart
@@ -0,0 +1,31 @@
+enum LoginStatus { initial, loading, success, error }
+
+class LoginState {
+  final LoginStatus status;
+  final String? error;
+  final String? accessToken;
+  final String? refreshToken;
+
+  LoginState._({
+    required this.status,
+    this.error,
+    this.accessToken,
+    this.refreshToken,
+  });
+
+  factory LoginState.initial() => LoginState._(status: LoginStatus.initial);
+
+  LoginState copyWith({
+    LoginStatus? status,
+    String? error,
+    String? accessToken,
+    String? refreshToken,
+  }) {
+    return LoginState._(
+      status: status ?? this.status,
+      error: error ?? this.error,
+      accessToken: accessToken ?? this.accessToken,
+      refreshToken: refreshToken ?? this.refreshToken,
+    );
+  }
+}
