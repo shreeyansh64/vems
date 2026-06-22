0a39191d01756e32a059611471346c5d602c5412
shreeyansh64 <shreeyansh64@gmail.com>
2026-06-22 21:07:01 +0530

    docs(data): add repository impl

---
+++ lib/features/documents/data/repository/document_repository_impl.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+import 'package:vems/features/documents/data/data_source/document_remote.dart';
+import 'package:vems/features/documents/domain/model/document_model.dart';
+import 'package:vems/features/documents/domain/repository/document_repository.dart';
+
+class DocumentRepositoryImpl implements DocumentRepository {
+  final DocumentRemote documentRemote;
+  DocumentRepositoryImpl({required this.documentRemote});
+
+  @override
+  Future<DocumentModel> uploadDocument(String documentType, File file) {
+    return documentRemote.uploadDocument(documentType, file);
+  }
+}
