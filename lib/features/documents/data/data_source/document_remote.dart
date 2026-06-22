e5c6e811c8aee4e5aa720350133d129f99d3e417
shreeyansh64 <shreeyansh64@gmail.com>
2026-06-22 21:05:54 +0530

    docs(data): add doc remote data source

---
+++ lib/features/documents/data/data_source/document_remote.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+import 'package:dio/dio.dart';
+import 'package:vems/features/documents/domain/model/document_model.dart';
+
+class DocumentRemote {
+  final Dio dio;
+  DocumentRemote({required this.dio});
+
+  Future<DocumentModel> uploadDocument(String documentType, File file) async {
+    try {
+      final formData = FormData.fromMap({
+        'document_type': documentType,
+        'file': await MultipartFile.fromFile(
+          file.path,
+          filename: file.path.split('/').last,
+        ),
+      });
+
+      final response = await dio.post('/api/documents/upload/', data: formData);
+      return DocumentModel.fromJson(response.data as Map<String, dynamic>);
+    } on DioException catch (e) {
+      throw e.response?.data['document_type'] ??
+          e.response?.data['file'] ??
+          e.response?.data['message'] ??
+          "Something went wrong";
+    }
+  }
+}
--- lib/features/documents/domain/model/document_model.dart
+++ lib/features/documents/domain/model/document_model.dart
@@ -10,4 +10,13 @@ class DocumentModel {
     required this.ocrStatus,
     required this.verificationStatus,
   });
+
+  factory DocumentModel.fromJson(Map<String, dynamic> json) {
+    return DocumentModel(
+      id: json['id'],
+      user: json['user'],
+      ocrStatus: json['ocr_status'],
+      verificationStatus: json['verification_status'],
+    );
+  }
 }
