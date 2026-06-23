27470f39de8e0588f525f05369504c7b9c6e0076
shreeyansh64 <shreeyansh64@gmail.com>
2026-06-23 22:27:31 +0530

    scanner: add scanner for staff and fix(routing)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.vems"
+    namespace = "com.shreeyansh.vems"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.vems"
+        applicationId = "com.shreeyansh.vems"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
@@ -30,11 +30,19 @@ android {
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("/home/zeus/secrets/vems-release-key.jks")
+            keyAlias = "vems"
+        }
+    }
+
     buildTypes {
         release {
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
--- android/app/src/main/kotlin/com/shreeyansh/vems/MainActivity.kt
+++ android/app/src/main/kotlin/com/shreeyansh/vems/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.vems
+package com.shreeyansh.vems
 
 import io.flutter.embedding.android.FlutterActivity
 
